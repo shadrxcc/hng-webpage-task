1220a79df0678d9c02d0dc91496abe9002b7be42
shadrach akaade <akaadesm@gmail.com>
2022-10-29 21:10:43 +0100

    format files and add linter files

--- src/components/links.js
+++ src/components/links.js
@@ -1,90 +1,90 @@
 import React from "react";
 
 const Links = () => {
-    return (
-      <>
-        <div className="nav">
-          <div className="link-tag">
-            <a
-              className="links"
-              id="btn-zuri"
-              target="blank"
-              href="https://twitter.com/shadrxcc/"
-            >
-              Twitter Link
-            </a>
-          </div>
-
-          <div className="link-tag">
-            <a
-              className="links"
-              id="btn-zuri"
-              target="blank"
-              href="https://training.zuri.team/"
-            >
-              Zuri Team
-            </a>
-          </div>
-
-          <div className="link-tag">
-            <a
-              className="links"
-              id="books"
-              target="blank"
-              href="http://books.zuri.team/"
-            >
-              Zuri Books
-              <sub hidden>
-                A directory where you find books about design and coding
-              </sub>
-            </a>
-          </div>
-
-          <div className="link-tag">
-            <a
-              className="links"
-              id="book__python"
-              target="blank"
-              href="https://books.zuri.team/python-for-beginners?ref_id=<Shadrach Akaade>"
-            >
-              Python Books
-              <sub hidden>
-                A directory where you feature the book you want to selling
-              </sub>
-            </a>
-          </div>
-
-          <div className="link-tag">
-            <a
-              className="links"
-              id="pitch"
-              target="blank"
-              href="https://background.zuri.team/"
-            >
-              Background Check for Coders
-              <sub hidden>
-                A directory where you pitch your services of doing background
-                checks on coders
-              </sub>
-            </a>
-          </div>
-
-          <div className="link-tag">
-            <a
-              className="links"
-              id="book__design"
-              target="blank"
-              href="https://books.zuri.team/design-rules"
-            >
-              Design Books
-              <sub hidden>
-                A directory where you pitch the free design book offered by Zuri
-              </sub>
-            </a>
-          </div>
+  return (
+    <>
+      <div className="nav">
+        <div className="link-tag">
+          <a
+            className="links"
+            id="btn-zuri"
+            target="blank"
+            href="https://twitter.com/shadrxcc/"
+          >
+            Twitter Link
+          </a>
         </div>
-      </>
-    );
-}
 
-export default Links
+        <div className="link-tag">
+          <a
+            className="links"
+            id="btn-zuri"
+            target="blank"
+            href="https://training.zuri.team/"
+          >
+            Zuri Team
+          </a>
+        </div>
+
+        <div className="link-tag">
+          <a
+            className="links"
+            id="books"
+            target="blank"
+            href="http://books.zuri.team/"
+          >
+            Zuri Books
+            <sub hidden>
+              A directory where you find books about design and coding
+            </sub>
+          </a>
+        </div>
+
+        <div className="link-tag">
+          <a
+            className="links"
+            id="book__python"
+            target="blank"
+            href="https://books.zuri.team/python-for-beginners?ref_id=<Shadrach Akaade>"
+          >
+            Python Books
+            <sub hidden>
+              A directory where you feature the book you want to selling
+            </sub>
+          </a>
+        </div>
+
+        <div className="link-tag">
+          <a
+            className="links"
+            id="pitch"
+            target="blank"
+            href="https://background.zuri.team/"
+          >
+            Background Check for Coders
+            <sub hidden>
+              A directory where you pitch your services of doing background
+              checks on coders
+            </sub>
+          </a>
+        </div>
+
+        <div className="link-tag">
+          <a
+            className="links"
+            id="book__design"
+            target="blank"
+            href="https://books.zuri.team/design-rules"
+          >
+            Design Books
+            <sub hidden>
+              A directory where you pitch the free design book offered by Zuri
+            </sub>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Links;
