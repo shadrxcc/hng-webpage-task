958a264396e72af9e94bd4541467c43b5a117cbc
shadrach akaade <akaadesm@gmail.com>
2022-10-29 21:06:25 +0100

    implement share button and adjust styling

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import { Icon } from "@iconify/react";
 
 const ProfileImg = () => {
   return (
     <>
       <div className="pfp">
+        <div className="menu-share">
+          <span className="menu">
+            <Icon className="iconrct" icon="carbon:overflow-menu-horizontal" />
+          </span>
+          <span className="share">
+            <Icon className="iconrct" icon="bx:share" hFlip={true} />
+          </span>
+        </div>
         <div>
           <img
-            className="pic"
+            id="profile__img"
             src="https://www.linkpicture.com/q/logo-1_1.jpg"
             alt="profile-pic"
           ></img>
         </div>
         <div>
-          <h3 className='username'>Shadrach Akaade</h3>
+          <h3 id="twitter">shadrxcc</h3>
+        </div>
+        <div>
+          <h3 id="slack" hidden>
+            sheddy sk.
+          </h3>
         </div>
       </div>
     </>
